e0203713821ab7ac59b317cb926845a09fb899c3
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-06-29 20:21:22 +0300

    is  rdy

--- src/Fealadat_1.java
+++ src/Fealadat_1.java
@@ -1,9 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Fealadat_1 {
     public static void main(String[] args) {
-
+        System.out.println(FileInputHandlerr.getLinesOfTextFileWithScanner("peldaStrings.txt"));
     }
 }
 
-// Írj egy osztályt mely tartalmaz egy fájlból olvasó statitikus metódust ami bemeneti paraméterként vár egy file pathot
+// Ez az osztály felel azért hogy lekezeljük a bejövő fájlt
+class FileInputHandlerr {
+
+    public static List<String> getLinesOfTextFileWithScanner(String filePath) {
+        List<String> stringList = new ArrayList<>();
+
+        File file = new File(filePath);
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                stringList.add(sc.nextLine());
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return stringList;
+    }
+}
+
+
+// Írj egy osztályt mely tartalmaz egy fájlból olvasó statitikus metódust
+// ami bemeneti paraméterként vár egy file pathot
 // a metódus térjen vissza egy String listával aminek elemei a fájl sorai legyenek
 // hívd meg a metódusod peldaString.txt re és irasd ki az eredményt
--- src/Feladat_2.java
+++ src/Feladat_2.java
@@ -1,10 +1,67 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Feladat_2 {
     public static void main(String[] args) {
+        List<String> list = FileInputHandler_2.getLinesOfTextFile("szamok.txt");
+        int sum = 0;
+        for (var actual : list) {
+            for (var actualNumber : actual.split(" ")) {
+                sum += Integer.parseInt(actualNumber);
 
+            }
+        }
+        System.out.println(sum);
+    }
+
+    static class FileInputHandler_2 {
+
+        public static List<String> getLinesOfTextFileWithScanner(String filePath) {
+            List<String> stringList = new ArrayList<>();
+
+            File file = new File(filePath);
+            Scanner sc = null;
+            try {
+                sc = new Scanner(file);
+                while (sc.hasNextLine()) {
+                    stringList.add(sc.nextLine());
+                }
+                sc.close();
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            return stringList;
+        }
+
+
+        public static List<String> getLinesOfTextFile(String filePath) {
+            List<String> stringList = new ArrayList<>();
+
+
+            try {
+                FileReader fileReader = new FileReader(filePath); // IOExceptiont dob ha nem található a file a megadott útvonalon
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+                // itt végig megyünk a sorokon és hozzá adjuk a listához
+                for (String line; (line = bufferedReader.readLine()) != null; ) {
+                    stringList.add(line);
+                }
+
+
+            } catch (IOException e) {
+                System.out.println("Error: wrong path!\n" + e.getMessage());
+            }
+            return stringList;
+        }
     }
 }
 
-// Írj egy osztályt mely tartalmaz egy fájlból olvasó satitikus metódust ami bemeneti paraméterként vár egy file pathot
-// a metódus térjen vissza egy String listával aminek elemei a fájl sorai legyenek
+// Írj egy osztályt mely tartalmaz egy fájlból olvasó satitikus metódust
+// ami bemeneti paraméterként vár egy file pathot
+// a metódus térjen vissza egy String listával aminek elemei
+// a fájl sorai legyenek
 // hívd meg a metódusod szamok.txt re és irasd ki az eredményt
-// az eredményed számokat tartalmaz szóközökkel elválasztva írasd ki a számok összegét
+// az eredményed számokat tartalmaz szóközökkel
+// elválasztva írasd ki a számok összegét
